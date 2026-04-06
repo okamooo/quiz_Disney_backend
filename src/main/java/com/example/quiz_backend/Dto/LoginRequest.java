bc645e8b21cdf6f2395ba61079d7eb8c23a86352
yuria <yr.work12@gmail.com>
2026-04-06 15:21:15 +0900

    コメント付け直し

--- src/main/java/com/example/quiz_backend/Dto/LoginRequest.java
+++ src/main/java/com/example/quiz_backend/Dto/LoginRequest.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class LoginRequest {
-    private String loginId;
-    private String password;
+    private String loginId; // メール / ユーザーID
+    private String password; // パスワード
 }
