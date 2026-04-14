574d13d5de51268b7131f83504d4ccb07701dccf
Takuya Okamoto <taku@TakuyanoMacBook-Pro.local>
2026-04-14 11:43:55 +0900

    quizmodeの処理を追加

--- src/main/java/com/example/quiz_backend/dto/QuizMode.java
+++ src/main/java/com/example/quiz_backend/dto/QuizMode.java
@@ -1,5 +1,7 @@
 package com.example.quiz_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -7,4 +9,7 @@ public class QuizMode {
     private String quizMode;
     private String quizModeLabel;
     private String quizStartUrl;
+
+    @JsonProperty("isAvailable") // JSONのキーを "isAvailable" にする
+    private boolean isAvailable; // クイズが利用可能かどうか
 }
